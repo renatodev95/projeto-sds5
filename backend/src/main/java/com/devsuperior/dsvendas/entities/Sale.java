a6788638ddda1c346b0d12509650fa1253f2b822
Renato Pereira Santos <renatodev95@gmail.com>
2021-11-03 11:52:14 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -0,0 +1,83 @@
+package com.devsuperior.dsvendas.entities;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private Long visited;
+    private Long deals;
+    private Double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+    public Sale() {
+    }
+
+    public Sale(Long id, Long visited, Long deals, Double amount, LocalDate date, Seller seller) {
+        this.id = id;
+        this.visited = visited;
+        this.deals = deals;
+        this.amount = amount;
+        this.date = date;
+        this.seller = seller;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Long visited) {
+        this.visited = visited;
+    }
+
+    public Long getDeals() {
+        return deals;
+    }
+
+    public void setDeals(Long deals) {
+        this.deals = deals;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+}
